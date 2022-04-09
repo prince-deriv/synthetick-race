212d81e2d1d73228643504426e22d0fd1f19b4f9
Prince Coching <rencechi05@gmail.com>
2022-04-09 14:22:58 +0400

    chore: add title and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,24 @@
 module.exports = {
   siteMetadata: {
     title: ``,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-resolve-src",
-  ],
-};
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-resolve-src',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Synthetick Race',
+        short_name: 'synthetick-rade',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone',
+        icon: 'src/images/cars/car-red.png'
+      }
+    }
+  ]
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
-import React, { useRef, useEffect, useState } from "react";
-import styled from "styled-components";
-import "styles/global.scss";
-import TerrainBg from "images/terrain.jpg";
-import { Car, Indicator, Obstacle } from "components";
-import { useRecoilState } from "recoil";
-import { speedState } from "atoms";
-import { getSpeed, getState, setDistance } from "helpers/utils";
-import ReactSpeedometer from "react-d3-speedometer";
-import Menu, { FooterMenu } from "components/menu";
-import { gameState } from "atoms";
-import { useRecoilValue } from "recoil";
+import React, { useRef, useEffect, useState } from 'react'
+import styled from 'styled-components'
+import 'styles/global.scss'
+import TerrainBg from 'images/terrain.jpg'
+import { Car, Indicator, Obstacle } from 'components'
+import { useRecoilState } from 'recoil'
+import { speedState } from 'atoms'
+import { getSpeed, getState, setDistance } from 'helpers/utils'
+import ReactSpeedometer from 'react-d3-speedometer'
+import Menu, { FooterMenu } from 'components/menu'
+import { gameState } from 'atoms'
+import { Helmet } from 'react-helmet'
 
 const Land = styled.div`
   position: absolute;
   top: 0px;
   width: 100%;
   height: 100%;
-`;
+`
 
 const Terrain = styled.div`
   position: absolute;
@@ -32,7 +32,7 @@ const Terrain = styled.div`
   background-position: 0 0;
   z-index: 1;
 }
-`;
+`
 
 const SpeedoMeterContainer = styled.div`
   position: absolute;
@@ -40,7 +40,7 @@ const SpeedoMeterContainer = styled.div`
   width: 100px;
   height: 100px;
   right: 82px;
-`;
+`
 
 const PlaceBox = styled.div`
   position: absolute;
@@ -57,7 +57,7 @@ const PlaceBox = styled.div`
   color: white;
   padding: 8px 8px 0px 8px;
   border-radius: 5px 0px 0px 0px;
-`;
+`
 
 const NumberBox = styled.div`
   position: absolute;
@@ -96,7 +96,7 @@ const NumberBox = styled.div`
       color: #000;
     }
   }
-`;
+`
 
 const ScoreBoard = styled.div`
   position: absolute;
@@ -148,136 +148,142 @@ const ScoreBoard = styled.div`
       text-transform: uppercase;
     }
   }
-`;
+`
 
 const Game = () => {
-  const [game_state, setGameState] = useRecoilState(gameState);
+  const [game_state, setGameState] = useRecoilState(gameState)
 
-  const terrain = useRef();
-  const [speed, setCarSpeed] = useRecoilState(speedState);
+  const terrain = useRef()
+  const [speed, setCarSpeed] = useRecoilState(speedState)
   const [data, setData] = useState({
-    speed: 0,
-  });
+    speed: 0
+  })
 
   useEffect(() => {
-    if (game_state === "race") {
-      const current = terrain.current;
-      let terrain_move;
+    if (game_state === 'race') {
+      const current = terrain.current
+      let terrain_move
       if (current) {
         terrain_move = setInterval(() => {
-          move();
-        }, 10);
+          move()
+        }, 10)
       }
 
-      return () => clearInterval(terrain_move);
+      return () => clearInterval(terrain_move)
     }
-  }, [game_state]);
+  }, [game_state])
 
   const move = () => {
-    const current = terrain.current;
+    const current = terrain.current
 
-    const bpy = current.style.backgroundPositionY;
+    const bpy = current.style.backgroundPositionY
 
-    const speed = getSpeed("car");
-    const posY = bpy === "" ? 0 : parseFloat(bpy);
+    const speed = getSpeed('car')
+    const posY = bpy === '' ? 0 : parseFloat(bpy)
 
-    const new_bpy = posY + speed;
+    const new_bpy = posY + speed
 
-    const status = getState("car");
+    const status = getState('car')
 
-    if (status !== "finished") {
-      current.style.backgroundPositionY = `${new_bpy}px`;
+    if (status !== 'finished') {
+      current.style.backgroundPositionY = `${new_bpy}px`
     }
 
-    const new_data = { ...data };
-    new_data.speed = parseInt(speed * 25);
+    const new_data = { ...data }
+    new_data.speed = parseInt(speed * 25)
 
-    setDistance("terrain", new_bpy);
-    setCarSpeed(speed);
-    setData(new_data);
-  };
+    setDistance('terrain', new_bpy)
+    setCarSpeed(speed)
+    setData(new_data)
+  }
 
   return (
-    <Land>
-      <Terrain ref={terrain} id="terrain">
-        <Indicator />
-        <SpeedoMeterContainer>
-          <ReactSpeedometer
-            maxValue={300}
-            width={175}
-            height={170}
-            value={data.speed}
-            segments={5}
-            needleColor="#ffffff"
-            needleTransition="easeBounceIn"
-            labelFontSize={"0px"}
-            valueTextFontSize="75px"
-            textColor="#ffffff"
-            segmentColors={[
-              "#111111",
-              "#222222",
-              "#333333",
-              "#444444",
-              "#555555",
-            ]}
-          />
-        </SpeedoMeterContainer>
-        <NumberBox>
-          <span className="number-item">0</span>
-          <span className="number-item">1</span>
-          <span className="number-item">2</span>
-          <span className="number-item">3</span>
-          <span className="number-item">4</span>
-          <span className="number-item">5</span>
-          <span className="number-item">6</span>
-          <span className="number-item">7</span>
-          <span className="number-item">8</span>
-          <span className="number-item">9</span>
-        </NumberBox>
-        <PlaceBox>
-          <strong id="place"></strong>
-        </PlaceBox>
-      </Terrain>
-      <Obstacle />
-      <Car />
-      <ScoreBoard id="score-board">
-        <span className="title">Result</span>
-        <div className="item">
-          <span className="place">1st</span>
-          <span className="name" id="place-0"></span>
-        </div>
-        <div className="item">
-          <span className="place">2nd</span>
-          <span className="name" id="place-1"></span>
-        </div>
-        <div className="item">
-          <span className="place">3rd</span>
-          <span className="name" id="place-2"></span>
-        </div>
-        <div className="item">
-          <span className="place">4th</span>
-          <span className="name" id="place-3"></span>
-        </div>
-        <div className="item">
-          <span className="place">5th</span>
-          <span className="name" id="place-4"></span>
-        </div>
-        <div className="item">
-          <span className="place">6th</span>
-          <span className="name" id="place-5"></span>
-        </div>
-        <FooterMenu>
-          <span
-            className="button"
-            onClick={() => window && window.location.reload()}
-          >
-            ← Home
-          </span>
-        </FooterMenu>
-      </ScoreBoard>
-      <Menu />
-    </Land>
-  );
-};
+    <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Synthetick Race</title>
+      </Helmet>
+      <Land>
+        <Terrain ref={terrain} id='terrain'>
+          <Indicator />
+          <SpeedoMeterContainer>
+            <ReactSpeedometer
+              maxValue={300}
+              width={175}
+              height={170}
+              value={data.speed}
+              segments={5}
+              needleColor='#ffffff'
+              needleTransition='easeBounceIn'
+              labelFontSize={'0px'}
+              valueTextFontSize='75px'
+              textColor='#ffffff'
+              segmentColors={[
+                '#111111',
+                '#222222',
+                '#333333',
+                '#444444',
+                '#555555'
+              ]}
+            />
+          </SpeedoMeterContainer>
+          <NumberBox>
+            <span className='number-item'>0</span>
+            <span className='number-item'>1</span>
+            <span className='number-item'>2</span>
+            <span className='number-item'>3</span>
+            <span className='number-item'>4</span>
+            <span className='number-item'>5</span>
+            <span className='number-item'>6</span>
+            <span className='number-item'>7</span>
+            <span className='number-item'>8</span>
+            <span className='number-item'>9</span>
+          </NumberBox>
+          <PlaceBox>
+            <strong id='place'></strong>
+          </PlaceBox>
+        </Terrain>
+        <Obstacle />
+        <Car />
+        <ScoreBoard id='score-board'>
+          <span className='title'>Result</span>
+          <div className='item'>
+            <span className='place'>1st</span>
+            <span className='name' id='place-0'></span>
+          </div>
+          <div className='item'>
+            <span className='place'>2nd</span>
+            <span className='name' id='place-1'></span>
+          </div>
+          <div className='item'>
+            <span className='place'>3rd</span>
+            <span className='name' id='place-2'></span>
+          </div>
+          <div className='item'>
+            <span className='place'>4th</span>
+            <span className='name' id='place-3'></span>
+          </div>
+          <div className='item'>
+            <span className='place'>5th</span>
+            <span className='name' id='place-4'></span>
+          </div>
+          <div className='item'>
+            <span className='place'>6th</span>
+            <span className='name' id='place-5'></span>
+          </div>
+          <FooterMenu>
+            <span
+              className='button'
+              onClick={() => window && window.location.reload()}
+            >
+              ← Home
+            </span>
+          </FooterMenu>
+        </ScoreBoard>
+        <Menu />
+      </Land>
+    </>
+  )
+}
 
-export default Game;
+export default Game
